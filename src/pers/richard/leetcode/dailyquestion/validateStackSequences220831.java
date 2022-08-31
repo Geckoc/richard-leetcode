7248f4ba515489ad45757a57897d153022bc4f93
gecko <geckochanilix@gmail.com>
2022-08-31 16:23:24 +0800

    FileUpdate

---
+++ src/pers/richard/leetcode/dailyquestion/validateStackSequences220831.java
@@ -0,0 +1,10 @@
+package pers.richard.leetcode.dailyquestion;
+
+/**
+ * @ClassName: validateStackSequences220831
+ * @Description:
+ * @Author: Richard_Chen
+ * @Create: 2022-08-31 16:22
+ */
+public class validateStackSequences220831 {
+}
