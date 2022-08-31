1c5050b9b4f8701b68e86b40052995267d884328
gecko <geckochanilix@gmail.com>
2022-08-31 10:28:12 +0800

    Connection Test

---
+++ src/com/richard/leetcode/brinarySearch/BinarySearchModel1.java
@@ -0,0 +1,21 @@
+package com.richard.leetcode.brinarySearch;
+
+/**
+ * @ClassName: Solution
+ * @Description: 二分查找
+ *  LeetCode - LeetBook
+ *  
+ * @Author: Richard_Chen
+ * @Create: 2022-08-31 10:11
+ */
+public class BinarySearchModel1 {
+
+    /**
+     * x的平方根
+     * @return x
+     */
+    public int mySqrt(){
+
+        return -1;
+    }
+}
