4e14eb848cb3706dfa0e3b86321850ec1b2b0538
gecko <geckochanilix@gmail.com>
2022-09-16 10:06:16 +0800

    优美的排列II

--- src/pers/richard/leetcode/dailyquestion/sep22/Day08ConstructArray.java
+++ src/pers/richard/leetcode/dailyquestion/sep22/Day08ConstructArray.java
@@ -1,7 +1,7 @@
 package pers.richard.leetcode.dailyquestion.sep22;
 
 /**
- * @ClassName: ConstructArray08
+ * @ClassName: Day08ConstructArray
  * @Description: 优美的排列 II
  * 给你两个整数 n 和 k ，请你构造一个答案列表 answer ，该列表应当包含从 1 到 n 的 n 个不同正整数，并同时满足下述条件：
  * 假设该列表是 answer = [a1, a2, a3, ... , an] ，那么列表 [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] 中应该有且仅有 k 个不同整数。
@@ -23,7 +23,7 @@ package pers.richard.leetcode.dailyquestion.sep22;
  * @Author: Richard_Chen
  * @Create: 2022-09-08 18:08
  */
-public class ConstructArray08 {
+public class Day08ConstructArray {
 
     /**
      *
