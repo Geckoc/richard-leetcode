de527518b1093ae6685b764969ffa8813e872706
gecko <geckochanilix@gmail.com>
2022-09-16 10:02:40 +0800

    二进制矩阵中的特殊位置

--- src/pers/richard/leetcode/dailyquestion/sep22/Day04NumSpecial.java
+++ src/pers/richard/leetcode/dailyquestion/sep22/Day04NumSpecial.java
@@ -1,7 +1,7 @@
 package pers.richard.leetcode.dailyquestion.sep22;
 
 /**
- * @ClassName: NumSpecial04
+ * @ClassName: Day04NumSpecial
  * @Description: 二进制矩阵中的特殊位置
  * 给你一个大小为 rows x cols 的矩阵 mat，其中 mat[i][j] 是 0 或 1，请返回 矩阵 mat 中特殊位置的数目 。
  * 特殊位置 定义：如果 mat[i][j] == 1 并且第 i 行和第 j 列中的所有其他元素均为 0（行和列的下标均 从 0 开始 ），则位置 (i, j) 被称为特殊位置。
@@ -39,7 +39,7 @@ package pers.richard.leetcode.dailyquestion.sep22;
  * @Author: Richard_Chen
  * @Create: 2022-09-08 18:14
  */
-public class NumSpecial04 {
+public class Day04NumSpecial {
 
     /**
      * 模拟
