3763a7bfb66cb55906282d59fafd815b5816523f
gecko <geckochanilix@gmail.com>
2022-09-30 09:58:31 +0800

    划分为K个相等的子集

---
+++ src/pers/richard/leetcode/dailyquestion/sep22/Day20CanPartitionKSubsets.java
@@ -0,0 +1,77 @@
+package pers.richard.leetcode.dailyquestion.sep22;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName: Day20CanPartitionKSubsets
+ * @Description: 划分为K个相等的子集
+ *
+ * 给定一个整数数组 nums 和一个正整数 k，找出是否有可能把这个数组分成 k 个非空子集，其总和都相等。
+ *
+ * 示例 1：
+ * 输入： nums = [4, 3, 2, 3, 5, 2, 1], k = 4
+ * 输出： True
+ * 说明： 有可能将其分成 4 个子集（5），（1,4），（2,3），（2,3）等于总和。
+ *
+ * 示例 2:
+ * 输入: nums = [1,2,3,4], k = 3
+ * 输出: false
+ *  
+ *
+ * 提示：
+ * 1 <= k <= len(nums) <= 16
+ * 0 < nums[i] < 10000
+ * 每个元素的频率在 [1,4] 范围内
+ * 通过次数87,929提交次数206,317
+ *
+ *
+ *
+ *
+ * @Author: Richard_Chen
+ * @Create: 2022-09-30 09:56
+ */
+public class Day20CanPartitionKSubsets {
+
+    int[] nums;
+    int per, n;
+    boolean[] dp;
+
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        this.nums = nums;
+        int all = Arrays.stream(nums).sum();
+        if (all % k != 0) {
+            return false;
+        }
+        per = all / k;
+        Arrays.sort(nums);
+        n = nums.length;
+        if (nums[n - 1] > per) {
+            return false;
+        }
+        dp = new boolean[1 << n];
+        Arrays.fill(dp, true);
+        return dfs((1 << n) - 1, 0);
+    }
+
+    public boolean dfs(int s, int p) {
+        if (s == 0) {
+            return true;
+        }
+        if (!dp[s]) {
+            return dp[s];
+        }
+        dp[s] = false;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] + p > per) {
+                break;
+            }
+            if (((s >> i) & 1) != 0) {
+                if (dfs(s ^ (1 << i), (p + nums[i]) % per)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
