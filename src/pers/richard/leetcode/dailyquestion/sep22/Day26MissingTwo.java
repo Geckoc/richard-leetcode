0b747edf312f9eb10c48d041921c61f4c83ecf90
gecko <geckochanilix@gmail.com>
2022-10-09 18:06:14 +0800

    消失的两个数字

---
+++ src/pers/richard/leetcode/dailyquestion/sep22/Day26MissingTwo.java
@@ -0,0 +1,54 @@
+package pers.richard.leetcode.dailyquestion.sep22;
+
+/**
+ * @ClassName: Day2MissingTwo
+ * @Description: 消失的两个数字
+ * 给定一个数组，包含从 1 到 N 所有的整数，但其中缺了两个数字。
+ * 你能在 O(N) 时间内只用 O(1) 的空间找到它们吗？
+ *
+ * 以任意顺序返回这两个数字均可。
+ *
+ * 示例 1:
+ * 输入: [1]
+ * 输出: [2,3]
+ * 示例 2:
+ *
+ * 输入: [2,3]
+ * 输出: [1,4]
+ * 提示：
+ *
+ * nums.length <= 30000
+ * @Author: Richard_Chen
+ * @Create: 2022-10-09 18:05
+ */
+public class Day26MissingTwo {
+
+    public int[] missingTwo(int[] nums) {
+        int xorsum = 0;
+        int n = nums.length + 2;
+        for (int num : nums) {
+            xorsum ^= num;
+        }
+        for (int i = 1; i <= n; i++) {
+            xorsum ^= i;
+        }
+        // 防止溢出
+        int lsb = (xorsum == Integer.MIN_VALUE ? xorsum : xorsum & (-xorsum));
+        int type1 = 0, type2 = 0;
+        for (int num : nums) {
+            if ((num & lsb) != 0) {
+                type1 ^= num;
+            } else {
+                type2 ^= num;
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            if ((i & lsb) != 0) {
+                type1 ^= i;
+            } else {
+                type2 ^= i;
+            }
+        }
+        return new int[]{type1, type2};
+    }
+}
