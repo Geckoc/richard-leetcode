b46322bb5ce616c3f91c1ab98fa4a45929599ee7
gecko <geckochanilix@gmail.com>
2022-10-10 09:26:10 +0800

    判定是否互为字符重排

---
+++ src/pers/richard/leetcode/dailyquestion/sep22/Day27CheckPermutation.java
@@ -0,0 +1,38 @@
+package pers.richard.leetcode.dailyquestion.sep22;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName: Day27CheckPermutation
+ * @Description: 判定是否互为字符重排
+ *
+ * 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+ *
+ * 示例 1：
+ * 输入: s1 = "abc", s2 = "bca"
+ * 输出: true
+ *
+ * 示例 2：
+ * 输入: s1 = "abc", s2 = "bad"
+ * 输出: false
+ *
+ * 说明：
+ * 0 <= len(s1) <= 100
+ * 0 <= len(s2) <= 100
+ *
+ * @Author: Richard_Chen
+ * @Create: 2022-10-10 09:24
+ */
+public class Day27CheckPermutation {
+
+    public boolean checkPermutation(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
