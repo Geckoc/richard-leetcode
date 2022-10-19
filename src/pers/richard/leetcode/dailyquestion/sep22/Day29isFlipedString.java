458c6408057b80755a56733614a4766b3453f03c
gecko <geckochanilix@gmail.com>
2022-10-19 17:46:24 +0800

    字符串轮转

---
+++ src/pers/richard/leetcode/dailyquestion/sep22/Day29isFlipedString.java
@@ -0,0 +1,30 @@
+package pers.richard.leetcode.dailyquestion.sep22;
+
+/**
+ * @ClassName: Day29isFlipedString
+ * @Description: 字符串轮转
+ * 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成
+ * （比如，waterbottle是erbottlewat旋转后的字符串）。
+ *
+ * 示例1:
+ *  输入：s1 = "waterbottle", s2 = "erbottlewat"
+ *  输出：True
+ *
+ * 示例2:
+ *  输入：s1 = "aa", s2 = "aba"
+ *  输出：False
+ *
+ * 提示：
+ * 字符串长度在[0, 100000]范围内。
+ * 说明:
+ * 你能只调用一次检查子串的方法吗？
+ *
+ * @Author: Richard_Chen
+ * @Create: 2022-10-19 17:43
+ */
+public class Day29isFlipedString {
+
+    public boolean isFlipedString(String s1, String s2) {
+        return s1.length() == s2.length() && (s1 + s1).contains(s2);
+    }
+}
